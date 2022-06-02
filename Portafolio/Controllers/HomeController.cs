64d451d96698e3c33955b91c0f5b0a1998e3a0a7
AlexW1993 <alexander_william93@hotmail.com>
2022-06-02 11:44:16 -0400

    Ajoutez des fichiers projet.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Servicios;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
+
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        {
+            _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
+        }
+
+        public IActionResult Index()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(2).ToList();
+            var modelo = new HomeIndexViewModel()
+            {
+                Proyectos = proyectos
+            };
+            return View(modelo);
+        }
+
+        public IActionResult Proyectos()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(Contacto contacto)
+        {
+            return RedirectToAction("Gracias",contacto);
+        }
+
+        public IActionResult Gracias(Contacto contacto)
+        {
+            return View(contacto);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,41 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+       
+        public List<Proyecto> ObtenerProyectos() 
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo =  "Critika",
+                    Descripcion = "Application Android pour faire des commentaire et des notes aux jeux vidéos",
+                    Link = "https://git.dti.crosemont.quebec/critika/clientandroid",
+                    ImageURL = "/imagenes/logoCritika.png"
+                },
+                new Proyecto
+                {
+                    Titulo =  "L'ombre des Purges",
+                    Descripcion = "Application Android d'une jeu de choix de rol avec plusieur choix à faire",
+                    Link = "https://git.dti.crosemont.quebec/yshmygun/app-dont-vous-etes-le-heros/-/tree/Livrable-TP3",
+                    ImageURL = "/imagenes/ombrePurges.png"
+                },
+                new Proyecto
+                {
+                    Titulo =  "Casino",
+                    Descripcion = "Application Android de simulation d'un casino",
+                    Link = "https://git.dti.crosemont.quebec/whernandez/casino/-/tree/TP3",
+                    ImageURL = "/imagenes/desCasino.png"
+                },
+            };
+        }
+
+    }
+}
